a3c7d4eabdc1548d4639af67b7e7722d258a9904
rahulkrishnan221 <rahulkrishnan221@gmail.com>
2017-12-08 05:16:42 +0530

    Google signin and added logout

--- app/src/main/java/com/jwes/flairs/flairs/Profile.java
+++ app/src/main/java/com/jwes/flairs/flairs/Profile.java
@@ -1,16 +1,42 @@
 package com.jwes.flairs.flairs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Profile extends AppCompatActivity {
+
+    private ImageButton logout;
+    private FirebaseAuth authen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        logout=(ImageButton) findViewById(R.id.logout);
+        authen =FirebaseAuth.getInstance();
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                out();
+            }
+        });
+
+    }
+    public void out()
+    {
+        authen.signOut();
+        if(authen.getCurrentUser() ==null)
+        {
+            startActivity(new Intent(Profile.this,option.class));
+        }
     }
 
 
--- app/src/main/java/com/jwes/flairs/flairs/loginf.java
+++ app/src/main/java/com/jwes/flairs/flairs/loginf.java
@@ -12,18 +12,32 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class loginf extends AppCompatActivity {
 
     private EditText txtEmailLogin;
+    FirebaseAuth.AuthStateListener mAuthListener;
     private EditText txtPwd;
-    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth mAuth;
     private LinearLayout forgot;
     private Button loginf;
+    private static final int RC_SIGN_IN=3;
+    SignInButton gsb;
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +45,9 @@ public class loginf extends AppCompatActivity {
         setContentView(R.layout.activity_loginf);
         txtEmailLogin = (EditText) findViewById(R.id.emailf);
         txtPwd = (EditText) findViewById(R.id.passwordf);
-        firebaseAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
         forgot=(LinearLayout) findViewById(R.id.forgot);
+        gsb=(SignInButton) findViewById(R.id.googlesb);
         loginf=(Button)findViewById(R.id.loginf) ;
         loginf.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +64,31 @@ public class loginf extends AppCompatActivity {
 
             }
         });
+
+        GoogleSignInOptions gso =new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Toast.makeText(loginf.this, "Somethhing went wrong", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        gsb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+
     }
 
     public void validate()
@@ -68,7 +108,7 @@ public class loginf extends AppCompatActivity {
     public void login() {
         final ProgressDialog progressDialog = ProgressDialog.show(loginf.this, "Please wait...", "Proccessing...", true);
 
-        (firebaseAuth.signInWithEmailAndPassword(txtEmailLogin.getText().toString(), txtPwd.getText().toString()))
+        (mAuth.signInWithEmailAndPassword(txtEmailLogin.getText().toString(), txtPwd.getText().toString()))
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -77,13 +117,63 @@ public class loginf extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             Toast.makeText(loginf.this, "Login successful", Toast.LENGTH_LONG).show();
                             Intent i = new Intent(loginf.this, MainActivity.class);
-                            i.putExtra("Email", firebaseAuth.getCurrentUser().getEmail());
+                            i.putExtra("Email", mAuth.getCurrentUser().getEmail());
                             startActivity(i);
                         } else {
                             Log.e("ERROR", task.getException().toString());
                             Toast.makeText(loginf.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
 
                         }
+                    }
+                });
+    }
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                Intent i=new Intent(loginf.this,MainActivity.class);
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+                startActivity(i);
+            } else {
+                Toast.makeText(this, "Auth went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(loginf.this,MainActivity.class));
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(loginf.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
                     }
                 });
     }
